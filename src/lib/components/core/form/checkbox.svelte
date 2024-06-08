479882c501659f0cb268585d54055a63f7934210
Ryan Zec <basire@gmail.com>
2024-06-08 12:51:43 -0400

    added explicit label property to checkbox

--- src/lib/components/core/form/checkbox.svelte
+++ src/lib/components/core/form/checkbox.svelte
@@ -1,17 +1,17 @@
 <script context="module" lang="ts">
   export type CheckboxValue = boolean | 'indeterminate';
-  export type OnChangeCheckboxArguments = { curr: CheckboxValue; next: CheckboxValue };
 </script>
 
 <script lang="ts">
   import { stringUtils } from '$lib/utils/string';
-  import SquareIcon from '../icons/square-icon.svelte';
-  import SquareCheckIcon from '../icons/square-check-icon.svelte';
+  import SquareIcon from '$lib/components/core/icons/square-icon.svelte';
+  import SquareCheckIcon from '$lib/components/core/icons/square-check-icon.svelte';
 
   export let checked: boolean;
   export let name: string;
   export let id: string = name;
   export let value: string;
+  export let label: string = value;
 </script>
 
 <label class="flex items-center" for={id} id="{value}-label">
@@ -23,5 +23,5 @@
     {/if}
   </div>
   <input {id} {name} type="checkbox" {value} {checked} class="appearance-none" />
-  {stringUtils.toTitleCase(value)}
+  {stringUtils.toTitleCase(label)}
 </label>
