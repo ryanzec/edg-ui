d67a97cd8b90ab8f8f208c9f8189886710060f49
Ryan Zec <basire@gmail.com>
2024-12-17 10:40:21 -0500

    updating to vitest beta to resolve issue related to using vite 6

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  // the typescript error does not actually effect anything so ignoring for now
-  // @ts-expect-error reference: https://github.com/sveltejs/kit/issues/10802
   plugins: [sveltekit()],
   test: { include: ['src/**/*.unit.{js,ts}'] },
   server: {
