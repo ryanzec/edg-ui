f44dd69e94bf474d7407e1e6b927a843f8446b3b
Ryan Zec <basire@gmail.com>
2024-12-24 14:54:50 -0500

    added code to reset classes for tailwind types that we are defining removed dead code from process design variables script

--- scripts/process-design-variables.ts
+++ scripts/process-design-variables.ts
@@ -1,16 +1,13 @@
 import * as fs from 'fs';
-// import { stringUtils } from '../src/lib/utils/string';
 
 const FIGMA_VARIABLES_JSON_PATH = './artifacts/figma-variables.json';
 const BASE_SIZE = 16;
 
 enum TemplatePath {
   CSS = './scripts/templates/app.css.template',
-  // TAILWIND_CONFIG = './scripts/templates/tailwind.config.js.template',
 }
 enum OutputPath {
   CSS = './src/app.css',
-  // TAILWIND_CONFIG = './tailwind.config.js',
 }
 enum ReplaceKey {
   LIGHT_CSS_VARIABLES = '%LIGHT_COLOR_VARIABLES%',
@@ -21,13 +18,6 @@ enum ReplaceKey {
   LINE_HEIGHT_CSS_VARIABLES = '%LINE_HEIGHT_VARIABLES%',
   Z_INDEX_CSS_VARIABLES = '%Z_INDEX_VARIABLES%',
   OPACITY_CSS_VARIABLES = '%OPACITY_VARIABLES%',
-  // TAILWIND_COLOR_VARIABLES = '%TAILWIND_COLOR_VARIABLES%',
-  // TAILWIND_SPACING_VARIABLES = '%TAILWIND_SPACING_VARIABLES%',
-  // TAILWIND_BORDER_RADIUS_VARIABLES = '%TAILWIND_BORDER_RADIUS_VARIABLES%',
-  // TAILWIND_SIZE_VARIABLES = '%TAILWIND_SIZE_VARIABLES%',
-  // TAILWIND_LINE_HEIGHT_VARIABLES = '%TAILWIND_LINE_HEIGHT_VARIABLES%',
-  // TAILWIND_Z_INDEX_VARIABLES = '%TAILWIND_Z_INDEX_VARIABLES%',
-  // TAILWIND_OPACITY_VARIABLES = '%TAILWIND_OPACITY_VARIABLES%',
 }
 
 type AliasVariable = {
@@ -96,36 +86,6 @@ const variableCollectionCssVariablePrefixMap: Record<VariableCollection, string>
   [VariableCollection.OPACITY]: 'opacity',
 };
 
-// type TailwindVariables = {
-//   colors: string[];
-//   spacing: string[];
-//   borderRadius: string[];
-//   fontSize: string[];
-//   lineHeight: string[];
-//   zIndex: string[];
-//   opacity: string[];
-// };
-
-// const variableCollectionTailwindVariableKeyMap: Record<VariableCollection, keyof TailwindVariables> = {
-//   [VariableCollection.COLOR]: 'colors',
-//   [VariableCollection.SPACE]: 'spacing',
-//   [VariableCollection.BORDER_RADIUS]: 'borderRadius',
-//   [VariableCollection.FONT_SIZE]: 'fontSize',
-//   [VariableCollection.LINE_HEIGHT]: 'lineHeight',
-//   [VariableCollection.Z_INDEX]: 'zIndex',
-//   [VariableCollection.OPACITY]: 'opacity',
-// };
-//
-// const tailwindVariables: TailwindVariables = {
-//   colors: [],
-//   spacing: [],
-//   borderRadius: [],
-//   fontSize: [],
-//   lineHeight: [],
-//   zIndex: [],
-//   opacity: [],
-// };
-
 const cleanVariableName = (variableName: string): string => variableName.replace(/\//g, '-');
 
 const cleanColorVariableName = (variableName: string): string => {
@@ -145,19 +105,16 @@ const buildColorCssVariables = (data: ConfigData): [string[], string[]] => {
     throw new Error('colors collection not found');
   }
 
-  const lightCssVariables: string[] = [];
-  const darkCssVariables: string[] = [];
   const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.COLOR];
+
+  // this default value basically reset the classes for any variable type we dynamically generate to avoid the
+  // default ones from being used and limit ourselves to just the ones we define
+  const lightCssVariables: string[] = [`  --${cssPrefix}-*: initial;`];
+  const darkCssVariables: string[] = [];
 
   for (const mode of colorCollection.modes) {
     for (const variable of mode.variables) {
       let cssVariableName = cleanColorVariableName(variable.name);
-
-      // we only need to process this once for tailwind
-      // if (mode.name === 'light') {
-      //   tailwindVariables.colors.push(cssVariableName);
-      // }
-
       const cssVariable = `  --${cssPrefix}-${cssVariableName}: ${variable.value};`;
 
       (mode.name === 'light' ? lightCssVariables : darkCssVariables).push(cssVariable);
@@ -179,16 +136,13 @@ const buildGeneralCssVariables = (data: ConfigData, variableCollection: Variable
   }
 
   const cssPrefix = variableCollectionCssVariablePrefixMap[variableCollection];
-  const cssVariables: string[] = [];
+
+  // this default value basically reset the classes for any variable type we dynamically generate to avoid the
+  // default ones from being used and limit ourselves to just the ones we define
+  const cssVariables: string[] = [`  --${cssPrefix}-*: initial;`];
 
   for (const variable of collection.modes[0].variables) {
     const variableName = cleanVariableName(variable.name);
-    // const tailwindVariableKey = variableCollectionTailwindVariableKeyMap[variableCollection];
-
-    // if (tailwindVariableKey) {
-    //   tailwindVariables[tailwindVariableKey as keyof typeof tailwindVariables].push(variableName);
-    // }
-
     const variableUnit = sizeCollections.includes(variableCollection) ? 'rem' : '';
     let variableRawValue = sizeCollections.includes(variableCollection)
       ? (variable.value as number) / BASE_SIZE
@@ -230,93 +184,8 @@ const writeCssFile = () => {
   fs.writeFileSync(OutputPath.CSS, cssTemplate);
 };
 
-// const buildTailwindVariableKey = (cssVariableName: string): string => {
-//   return cssVariableName.includes('-') || stringUtils.startsWithNumber(cssVariableName)
-//     ? `'${cssVariableName}'`
-//     : cssVariableName;
-// };
-//
-// const writeTailwindConfigurationFile = () => {
-//   const colorsThemeClasses: string[] = [];
-//
-//   tailwindVariables.colors.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.COLOR];
-//
-//     colorsThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const spacingThemeClasses: string[] = [];
-//
-//   tailwindVariables.spacing.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.SPACE];
-//
-//     spacingThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const borderRadiusThemeClasses: string[] = [];
-//
-//   tailwindVariables.borderRadius.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.BORDER_RADIUS];
-//
-//     borderRadiusThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const sizeThemeClasses: string[] = [];
-//
-//   tailwindVariables.fontSize.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.FONT_SIZE];
-//
-//     sizeThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const lineHeightThemeClasses: string[] = [];
-//
-//   tailwindVariables.lineHeight.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.LINE_HEIGHT];
-//
-//     lineHeightThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const zIndexThemeClasses: string[] = [];
-//
-//   tailwindVariables.zIndex.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.Z_INDEX];
-//
-//     zIndexThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   const opacityThemeClasses: string[] = [];
-//
-//   tailwindVariables.opacity.forEach((cssVariableName) => {
-//     const key = buildTailwindVariableKey(cssVariableName);
-//     const cssPrefix = variableCollectionCssVariablePrefixMap[VariableCollection.OPACITY];
-//
-//     opacityThemeClasses.push(`      ${key}: 'var(--${cssPrefix}-${cssVariableName})',`);
-//   });
-//
-//   let tailwindConfigTemplate = fs.readFileSync(TemplatePath.TAILWIND_CONFIG, 'utf8');
-//
-//   tailwindConfigTemplate = tailwindConfigTemplate
-//     .replace(ReplaceKey.TAILWIND_COLOR_VARIABLES, colorsThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_SPACING_VARIABLES, spacingThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_BORDER_RADIUS_VARIABLES, borderRadiusThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_LINE_HEIGHT_VARIABLES, lineHeightThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_Z_INDEX_VARIABLES, zIndexThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_OPACITY_VARIABLES, opacityThemeClasses.join('\n'))
-//     .replace(ReplaceKey.TAILWIND_SIZE_VARIABLES, sizeThemeClasses.join('\n'));
-//
-//   fs.writeFileSync(OutputPath.TAILWIND_CONFIG, tailwindConfigTemplate);
-// };
-
 try {
   writeCssFile();
-  // writeTailwindConfigurationFile();
 
   console.log('design variables processed successfully!');
 } catch (error) {
